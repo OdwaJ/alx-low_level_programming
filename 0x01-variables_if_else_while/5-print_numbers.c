b82eeff6210864ef6817b0db1680593a9b8214e0
OdwaJ <aojafta@gmail.com>
2023-06-27 10:08:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main (void)
+{
+int num;
+while (num< 12)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
